6b3f6ff6efeeee15ab98871690e9e2e05a61e083
Julian Piaggio <julianpiaggio@gmail.com>
2022-09-17 14:48:16 -0300

    BaSP-W05-2022 :load exercise 1 variables and operators

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,27 @@
+console.log('--EXCERCISE 1: VARIABLES Y OPERADORES');
+
+/* Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una
+3er variable.*/
+console.log('-- EXCERCISE 1.A:');
+
+var ageMark= 30;
+var agePeter= 28;
+var ageOverall = (ageMark + agePeter);
+console.log(ageOverall);
+
+/* Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable. */
+console.log('-- EXCERCISE 1.B:');
+
+var sentence = 'Sunday is';
+var complete = 'going to be a sunny day.';
+var sentenceComplete =(sentence + ' ' + complete);
+console.log(sentenceComplete);
+
+/* Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el
+resultado de la suma en una 3er variable (utilizar length).*/
+console.log('-- EXCERCISE 1.C:');
+
+var phrase = 'today is a day ';
+var phrase2 = 'that life is worth living';
+var unitedPhrase =(phrase + phrase2);
+console.log(unitedPhrase.length);
